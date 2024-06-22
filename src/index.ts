8d21820ea6dc24f5b2a23b9a508afa890931871e
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-22 22:58:20 +0600

    basic mongodb connect

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import express from "express"
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+
+
+
+
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+
+
+
+
+
+
