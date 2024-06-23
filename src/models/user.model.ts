3103504819f313ac1e1e477a71469f68f78cc553
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-23 08:41:06 +0600

    user schema add

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
-import mongoose, { Document } from "mongoose";
-
+import mongoose, { Document, Model, Schema } from "mongoose";
+const emailRegexPatten: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 export interface IUser extends Document {
     name: string;
@@ -9,10 +9,57 @@ export interface IUser extends Document {
         public_id: string;
         url: string
     };
-   role: string;
-   isVerified: boolean;
-   courses: Array<{courseId : string}>;
-   comparePassword: (password: string) => Promise<boolean>;
-   
+    role: string;
+    isVerified: boolean;
+    courses: Array<{ courseId: string }>;
+    comparePassword: (password: string) => Promise<boolean>;
+    SignAccessToken: () => string;
+    SignRefreshToken: () => string;
 }
 
+const userSchema: Schema<IUser> = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter your name"],
+            index: true
+        },
+        email: {
+            type: String,
+            required: [true, "Please enter your email"],
+            validate: {
+                validator: function (value: string) {
+                    return emailRegexPatten.test(value);
+                },
+                message: "Please enter a valid email",
+            },
+        },
+        password: {
+            type: String,
+            // required: [true, "Please enter your password"],  ->social auth not work
+            minlength: [6, "Password must be at least 6 characters"],
+        },
+        avatar: {
+            public_id: String,
+            url: String,
+        },
+        role: {
+            type: String,
+            default: "user",
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        courses: [
+            {
+                courseId: String,
+            },
+        ],
+    },
+    { timestamps: true }
+)
+
+
+
+export const userModel: Model<IUser> = mongoose.model("user", userSchema);
