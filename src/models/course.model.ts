58bd8538ae254beb3da027034b36efff7e3be6f8
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-23 21:05:04 +0600

    course schema done

--- src/models/course.model.ts
+++ src/models/course.model.ts
@@ -1,2 +1,104 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
+import { IUser } from "./user.model";
+
+
+//  comment schema
+
+interface IComment extends Document {
+    user: IUser;
+    question: string;
+    questionReplies?: IComment[]
+}
+
+const commentSchema = new Schema<IComment>({
+    user: Object,
+    question: String,
+    questionReplies: [Object]
+}, { timestamps: true });
+
+
+// Review schema
+
+interface IReview extends Document {
+    user: IUser;
+    rating: number;
+    comment: string;
+    commentReplies?: IComment[]
+}
+
+const reviewSchema = new Schema<IReview>({
+    user: Object,
+    rating: {
+        type: Number,
+        default: 0
+    },
+    comment: String,
+    commentReplies: [Object]
+}, { timestamps: true })
+
+
+
+// link schema
+
+interface ILink extends Document {
+    title: string;
+    url: string;
+}
+
+const linkSchema = new Schema<ILink>({
+    title: String,
+    url: String,
+});
+
+// course data schema
+
+interface ICourseData extends Document {
+    title: string;
+    description: string;
+    videoUrl: string;
+    videoThumbnail: object;
+    videoSection: string;
+    videoLength: number;
+    videoPlayer: string;
+    links: ILink[];
+    suggestion: string;
+    questions: IComment[];
+}
+
+const courseDataSchema = new Schema<ICourseData>({
+    title: String,
+    description: String,
+    videoUrl: String,
+    // videoThumbnail: Object,
+    videoSection: String,
+    videoLength: Number,
+    videoPlayer: String,
+    links: [linkSchema],
+    suggestion: String,
+    questions: [commentSchema]
+})
+
+// course
+
+export interface ICourse extends Document {
+    name: string;
+    description: string;
+    categories: string;
+    price: number;
+    estimatePrice?: number;
+    thumbnail: object;
+    tags: string;
+    level: string;
+    demoUrl: string;
+    benefits: {title: string}[];
+    prerequisites: { title: string }[];
+    reviews: IReview[];
+    courseData: ICourseData[];
+    rating?: number;
+    purchased: number;
+}
+
+
+
+
 
