b433ed314f9d165fc679316a99cf72dfe7941a5a
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-08-04 00:52:25 +0600

    auth update2 PATCH

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 
 app.use(express.json({ limit: "50mb" }));
